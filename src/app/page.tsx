fa2fd886dac34fbbb7a794b72f85b5af592046dc
jsambhav5 <jsambhav5@gmail.com>
2023-08-16 15:02:55 +0530

    Header Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
+"use client";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
-			<h1 className="text-blue-500">Hi From Sambhav Jain</h1>
-			<Image
-				width="1000"
-				height="500"
-				alt="logo"
-				src="/icons/jsambhav5-logo.png"
-			></Image>
+			<section id="hero">
+				<Hero />
+			</section>
 		</div>
 	);
 }
