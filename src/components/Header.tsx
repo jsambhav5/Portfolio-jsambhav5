c19d48974bc32c5e09b2d81d0a9aaaab0b245894
jsambhav5 <jsambhav5@gmail.com>
2023-08-16 13:59:13 +0530

    First Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+
+type Props = {};
+
+const Header = (props: Props) => {
+	return (
+		<header>
+			<div>{/*Social Icons*/}I am Header</div>
+		</header>
+	);
+};
+
+export default Header;
