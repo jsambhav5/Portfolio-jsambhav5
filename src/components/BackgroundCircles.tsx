322a519608301c6d68acf8ea056100ef2ac84007
jsambhav5 <jsambhav5@gmail.com>
2023-08-21 15:46:15 +0530

    Finalised Hero Section

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -19,7 +19,7 @@ const BackgroundCircles = (props: Props) => {
 			<div className="absolute border border-[#434343] rounded-full h-[300px] w-[300px] mt-52 animate-ping "></div>
 			<div className="absolute border border-[#434343] rounded-full h-[500px] w-[500px] mt-52 animate-ping "></div>
 			<div className="absolute border border-[#434343] rounded-full h-[650px] w-[650px] mt-52 animate-ping "></div>
-			<div className="absolute border border-[#F7AB0A] rounded-full h-[400px] w-[400px] md:h-[500px] md:w-[500px] lg:h-[500px] lg:w-[500px] mt-52 animate-pulse opacity-20"></div>
+			<div className="absolute border border-[#F7AB0A] rounded-full h-[400px] w-[400px] md:h-[550px] md:w-[550px] lg:h-[500px] lg:w-[500px] mt-52 animate-pulse opacity-20"></div>
 		</motion.div>
 	);
 };
