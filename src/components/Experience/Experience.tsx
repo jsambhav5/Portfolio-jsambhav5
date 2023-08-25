5060fc54e88afc32483f48ce54982593114381e3
jsambhav5 <jsambhav5@gmail.com>
2023-08-25 13:48:59 +0530

    Finalised the Experience Section

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -10,13 +10,13 @@ const Experience = (props: Props) => {
 			initial={{ opacity: 0 }}
 			whileInView={{ opacity: 1 }}
 			transition={{ duration: 1.5 }}
-			className="flex flex-col text-left md:text-left md:flex-row relative  max-w-5xl px-10 justify-center mx-auto items-center overflow-hidden h-auto"
+			className="flex flex-col text-left md:text-left md:flex-row relative  max-w-5xl px-5 md: px-10 justify-center mx-auto items-center overflow-hidden h-auto"
 		>
 			<h3 className="pl-5 absolute top-24 uppercase tracking-[20px] text-xl text-gray-500 ">
 				Experience
 			</h3>
 
-			<div className="w-full h-full flex space-x-5 p-10 snap-x snap-mandatory">
+			<div className="w-full h-full flex space-x-5 p-5 snap-x snap-mandatory">
 				{/* <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory"> */}
 				{experiences.map((experience) => (
 					<ExperienceCard
