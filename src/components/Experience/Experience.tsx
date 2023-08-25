50d235e0525be4fce6b2848885e57e5129f596bb
jsambhav5 <jsambhav5@gmail.com>
2023-08-25 13:35:52 +0530

    Finalised the Experience Section

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -10,9 +10,9 @@ const Experience = (props: Props) => {
 			initial={{ opacity: 0 }}
 			whileInView={{ opacity: 1 }}
 			transition={{ duration: 1.5 }}
-			className="flex flex-col text-left md:text-left md:flex-row relative h-screen max-w-5xl px-10 justify-evenly mx-auto items-center overflow-hidden"
+			className="flex flex-col text-left md:text-left md:flex-row relative  max-w-5xl px-10 justify-center mx-auto items-center overflow-hidden h-auto"
 		>
-			<h3 className="pl-5 absolute top-24 uppercase tracking-[20px] text-xl text-gray-500">
+			<h3 className="pl-5 absolute top-24 uppercase tracking-[20px] text-xl text-gray-500 ">
 				Experience
 			</h3>
 
--- src/components/Experience/ExperienceCard.tsx
+++ src/components/Experience/ExperienceCard.tsx
@@ -6,7 +6,7 @@ type Props = { data: any };
 
 const ExperienceCard = (props: Props) => {
 	return (
-		<article className="justify-center flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-full h-full mt-[150px] mb-[50px]  snap-center bg-[#292929] p-5 hover:opacity-100 opacity-70 cursor-pointer transition-opacity duration-200 overflow-hidden">
+		<article className="justify-center flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-full h-full mt-[120px] mb-[50px] lg:mt-[150px] lg:mb-[30px]  snap-center bg-[#292929] p-10 hover:opacity-100 opacity-70 cursor-pointer transition-opacity duration-200 overflow-hidden">
 			<motion.img
 				initial={{ y: -100, opacity: 0 }}
 				transition={{ duration: 1.2 }}
@@ -16,7 +16,7 @@ const ExperienceCard = (props: Props) => {
 				className="rounded-full w-[70px] h-[70px]  md:w-[90px] md:h-[90px] object-cover object-top"
 			/>
 			<div className="px-0 md:px-10">
-				<h4 className="text-4xl font-light">
+				<h4 className="text-3xl md:text-4xl font-light">
 					{props.data.designation}
 				</h4>
 				<p className="text-2xl font-bold mt-1 uppercase">
@@ -30,6 +30,7 @@ const ExperienceCard = (props: Props) => {
 							height="32"
 							width="32"
 							title={technology.name}
+							className="h-4 w-4 md:h-7 md:w-7 lg:h-10 lg:w-10"
 						/>
 					))}
 				</div>
@@ -37,7 +38,7 @@ const ExperienceCard = (props: Props) => {
 					{props.data.start} - {props.data.end}
 				</p>
 
-				<ul className="list-disc space-y-4 ml-5 text-lg">
+				<ul className="list-disc space-y-4 ml-5 text-sm lg:text-lg">
 					{props.data.summary.map((summaryPoint: string) => (
 						<li>{summaryPoint}</li>
 					))}
